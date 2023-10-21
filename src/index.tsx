a6f26aa211dfac6ac16ade184f7fc7a490f5fd04
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-10-21 16:16:47 +0800

    Update packages (#1)
    * chore(packages): update to latest non-breaking change package
    
    * chore(packages): update other packages to latest non-breaking change version
    
    * fix(typecheck): update code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import { guardAsDefined } from '@poolofdeath20/util';
+import { Optional } from '@poolofdeath20/util';
 
-const root = guardAsDefined({
-	value: document.getElementById('root'),
-	error: new Error(
-		'There should be an element with id of "root" in index.html'
-	),
-});
+const root = Optional.from(document.getElementById('root')).unwrapOrThrow(
+	new Error('There should be an element with id of "root" in index.html')
+);
+
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
 		<App />
