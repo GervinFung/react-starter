4ca9592b0c1390298dc8d473052d45f9164d9572
GervinFung <gervinfungdaxuen@gmail.com>
2024-09-09 00:37:54 +0800

    lint(knip): ignore `typescript-eslint`

--- knip.ts
+++ knip.ts
@@ -1,7 +1,12 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-	ignoreDependencies: ['eslint', '@eslint/compat', '@eslint/js'],
+	ignoreDependencies: [
+		'eslint',
+		'@eslint/compat',
+		'@eslint/js',
+		'typescript-eslint',
+	],
 };
 
 export default config;
