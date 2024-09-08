8107f8ddbe5c62f976f14b02c7783dc1bcfbcfe6
GervinFung <gervinfungdaxuen@gmail.com>
2024-09-09 00:28:42 +0800

    lint(knip): remove unnecessary packages

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-	ignoreDependencies: ['package.json'],
+	ignoreDependencies: ['eslint', '@eslint/compat', '@eslint/js'],
 };
 
 export default config;
